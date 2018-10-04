8793466c2dbde64dce8bbba31700c4c9132e3c4e
Ben Anderson <dataknut@icloud.com>
2018-10-04 22:49:17 +1300

    amended params & added git stub at end

--- analysis/runBoTY.R
+++ analysis/runBoTY.R
@@ -2,34 +2,45 @@
 # Master (make) file for documentation
 ####
 
+# --- load libraries ---
 library(hashTagR)
 
 # libs required by this code (.Rmd will call others needed there)
-reqLibs <- c("rmarkdown", "bookdown", "rtweet")
+reqLibs <- c("rmarkdown", "bookdown", "rtweet", "lubridate", "data.table")
 
 print(paste0("Loading the following libraries: ", reqLibs))
 # Use Luke's function to require/install/load
 hashTagR::loadLibraries(reqLibs)
 
+# --- set params ----
+projLoc <- hashTagR::findParentDirectory("hashTagR")
+hashtag <- "#birdoftheyear OR #boty" # see ?search_tweets - capitals are ignored (I think)
+oDfile <- paste0("~/Data/twitter/tw_", hashtag,"_", lubridate::now(),".csv")
+ofile <- paste0(projLoc, "/docs/birdOfTheYear2018.html")
+explHashTag <- 'https://twitter.com/hashtag/birdoftheyear' # <- explanatory link for the hashtag
+pubUrl <- paste0("https://dataknut.github.io/hashTagR/", ofile) # <- where the results are published
+rmdFile <- paste0(projLoc, "/analysis/birdOfTheYear2018.Rmd")
+
+
 # default code location - needed to load functions & parameters correctly so
 projLoc <- hashTagR::findParentDirectory("hashTagR")
 
-refresh <- 1 # 0 to skip data refresh
+refresh <- 0 # 0 to skip data refresh
+
+# --- code ---
 
 if(refresh){
-  # grab the recent data & add to what we've saved before
-  source(paste0(projLoc, "/dataProcessing/getBoTY.R"))
+  dt <- hashTagR::saveTweets(hashtag, oDfile)
+  message("Retreived ", nrow(dt), " tweets and saved them to ", ofile)
 }
 
-# --- set params ----
-ofile <- "/docs/birdOfTheYear2018.html"
-hashtag <- '#birdoftheyear OR #boty' # <- for a different string just edit this & re-run
-explHashTag <- 'https://twitter.com/hashtag/birdoftheyear' # <- explanatory link for the hashtag
-pubUrl <- paste0("https://dataknut.github.io/hashTagR/", ofile) # <- where the results are published
-
-rmdFile <- paste0(projLoc, "/analysis/birdOfTheYear2018.Rmd")
 rmarkdown::render(input = rmdFile,
                   output_format = "html_document2",
                   params = list(hashtag = hashtag, explHashTag = explHashTag, pubUrl = pubUrl),
-                  output_file = paste0(projLoc,ofile)
+                  output_file = ofile
 )
+
+# construct git commit
+cmsg <- paste0("Latest #birdOfTheYear data refresh & replot: ", lubridate::now())
+gc <- paste0("git commit -m ", cmsg)
+#try(system(gc))
