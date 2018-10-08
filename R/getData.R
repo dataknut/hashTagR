2c0992c5ed60e2908891762ed0afd3c7dd026742
Ben Anderson <ben@octomac.local>
2018-10-09 08:39:37 +1300

    Latest #birdOfTheYear data refresh & replot: 2018-10-09 08:39:37

--- R/getData.R
+++ R/getData.R
@@ -15,7 +15,7 @@
 #'
 #' @author Ben Anderson, \email{b.anderson@@soton.ac.uk}
 #' @export
-#'
+#' @family tweets
 saveTweets <- function(string,ofile, n = 18000){
   message("Looking for: ", string)
   t <- rtweet::search_tweets(
@@ -48,6 +48,7 @@ saveTweets <- function(string,ofile, n = 18000){
 #'
 #' @author Ben Anderson, \email{b.anderson@@soton.ac.uk}
 #' @export
+#' @family tweets
 #'
 
 loadTweets <- function(path, pattern, recursive = TRUE){
@@ -76,3 +77,32 @@ loadTweets <- function(path, pattern, recursive = TRUE){
     print(fb)
   }
 }
+
+#' Convert a list of hashtags to a Twitter search string
+#' 
+#' It's usually easier to keep the hashtags you want to search for in an R list. This function
+#' creates a proper API search using the operator you specify.
+#' 
+#' Do not include # in the list elements as this will be added automatically
+#' 
+#' @param ht the list of tags created using (e.g.) hashTags <- c("tag1", "tag2")
+#' @param op how to combine hashtag strings, default = OR
+#'
+#' @author Ben Anderson, \email{banderson@@soton.ac.uk}
+#' @export
+#' @family tweets
+
+createSearchFromTags <- function(ht, op = "OR"){
+  if(length(ht) == 0){
+    message("Nothing in the hasthtag list")
+  } else { # at least 1 hashtag
+    ss <- paste0("#",ht[1])
+    if(length(ht) > 1){ # at least 2
+      for(n in 2:length(ht)){
+        ss <- paste0(ss, " ", op, " #", ht[n]) 
+      }
+    }
+    return(ss)
+  }
+}
+
