ebd365f98656659397cd67132fbaa44fa8288363
Ben Anderson <ben@octomac.local>
2018-10-09 08:42:49 +1300

    added generic hashtag report stub and new script for Climate Change & Business conf

---
+++ analysis/runCCBC18.R
@@ -0,0 +1,55 @@
+# ### ----- About ----
+# Master (make) file for documentation
+####
+
+# --- load libraries ---
+library(hashTagR)
+
+# libs required by this code (.Rmd will call others needed there)
+reqLibs <- c("rmarkdown", "bookdown", "rtweet", "lubridate", "data.table")
+
+print(paste0("Loading the following libraries: ", reqLibs))
+# Use Luke's function to require/install/load
+hashTagR::loadLibraries(reqLibs)
+
+# --- set params ----
+refresh <- 0 # 0 to skip data refresh
+goGit <- 0 # 0 to skip git commit
+
+projLoc <- hashTagR::findParentDirectory("hashTagR") # <- project location
+hashtags <- c("edsclimatechat","ccbc18") # a list of the hashtags to search for - see ?search_tweets
+searchString <- hashTagR::createSearchFromTags(hashtags)
+
+oDfile <- paste0("~/Data/twitter/tw_", searchString,"_", lubridate::now(),".csv") # <- data file
+ofile <- paste0(projLoc, "/docs/ccbc2018.html") # <- html output file
+explHashTag <- "www.climateandbusiness.com" # <- explanatory link for the hashtag
+pubUrl <- paste0("https://dataknut.github.io/hashTagR/", ofile) # <- where the results are published
+rmdFile <- paste0(projLoc, "/analysis/genericHashTagReport.Rmd") # <- the Rmd code to render
+
+
+# --- code ---
+
+if(refresh){
+  dt <- hashTagR::saveTweets(searchString, oDfile)
+  message("Retreived ", nrow(dt), " tweets and saved them to ", oDfile)
+}
+
+rmarkdown::render(input = rmdFile,
+                  output_format = "html_document2",
+                  params = list(hashTags = hashtags, 
+                                searchString = searchString,
+                                explHashTag = explHashTag, 
+                                pubUrl = pubUrl),
+                  output_file = ofile
+)
+
+if(goGit){
+  # construct git commit
+  cmsg <- paste0("'Latest #ccbc2018 data refresh & replot: ", lubridate::now(), "'")
+  gc <- paste0("git commit -a -m ", cmsg)
+  try(system(gc))
+  gpl <- "git pull"
+  try(system(gpl))
+  gpu <- "git push origin refs/heads/master"
+  try(system(gpu))
+}
